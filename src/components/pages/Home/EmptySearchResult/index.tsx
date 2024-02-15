2e0d95e1e2c265fd09c96a9cd884a8fff455248f
Alesia Klemenia <klemeny_ap_20@mf.grsu.by>
2024-02-15 14:00:49 +0300

    feat: create 'no results' component

---
+++ src/components/pages/Home/EmptySearchResult/index.tsx
@@ -0,0 +1,37 @@
+import { Box, Typography } from '@mui/material';
+
+const EmptySearchResult = (): JSX.Element => {
+  return (
+    <Box
+      sx={{
+        width: '50vw',
+        minWidth: '300px',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        gap: '24px',
+      }}
+    >
+      <Typography variant="h3" textAlign="center" sx={{ margin: '48px' }}>
+        It seems, nothing was found :(
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          gap: '16px',
+        }}
+      >
+        <Typography variant="body1">What can you try to do?</Typography>
+        <Typography variant="body2">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}- Сheck
+          that you entered the correct request
+        </Typography>
+        <Typography variant="body2">- Start VPN</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default EmptySearchResult;
