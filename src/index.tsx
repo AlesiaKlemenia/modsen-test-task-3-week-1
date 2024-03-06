abdb1ff5e428d20c8b7c3103c2c824da644e5b72
Alesia Klemenia <klemeny_ap_20@mf.grsu.by>
2024-03-06 15:57:10 +0300

    feature: mui theming

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import '@/global.css';
 
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
 import router from '@/routes/routes';
+import theme from '@/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
